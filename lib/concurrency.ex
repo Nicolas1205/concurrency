f4320645d3b06a150c4a83d6fafe5c700d095198
Nicolas Daniel Anachuri <anachurinicolasdaniel@gmail.com>
2022-05-26 15:03:10 -0300

    Schedule.Scheduler working on

--- lib/concurrency.ex
+++ lib/concurrency.ex
@@ -1,22 +1,18 @@
-defprotocol Utility do 
-	@spec type(t) :: String.t() 
-	def type(value)
-end
+# TODO: so we have to change to configuration of a certain schedule 
+# which is concurrently running  
+# so we get to kill and restart that process with the new config 
 
-defimpl Utility, for: BitString do 
-	def type(_value), do: "String"
-end
-
-defimpl Utility, for: Integer do 
-	def type(value), do: value
-end
-
-
-defmodule Scheduler do 
+defmodule Schedule.Scheduler do 
 	# @derive {Inspect, only: [:task]}
 	# defstruct time: Schedule.Time, task: :nothing
-	
-	def update(ndt) do 
+
+	# So we have to run this process as supervisor 
+
+	# schedule: Schedule
+	def update_schedule(updated_schedule, pid) do 
+		send pid, :update
+		new_pid = spawn_link Concurrency, :match_schedule, [updated_schedule]
+		Map.get_and_update(updated_schedule, :pid, new_pid)
 		:ok
 	end
 end 
@@ -24,7 +20,6 @@ end
 # on_days: [1,2,3,4,5,6,7]
 # Date.day_of_wee ndt
 # Map.has_key? 
-
 
 defmodule Schedule.DB do 
 	def get_schedules do 
@@ -34,39 +29,48 @@ defmodule Schedule.DB do
 	end
 end
 
-defmodule Schedule.Time do 
-	@enforce_keys [:hour,:minute]
-	defstruct hour: :nothing, minute: :nothing
-end
-
 defmodule Concurrency do
   def main do
 	
 		{ok?, schedules} = Schedule.DB.get_schedules
+		pids = [1]
+		
 
+		# TODO: Implement Agent for stateful pids var 
 		case ok? do 
-			:ok -> Enum.map(schedules, fn schedule ->  spawn(Concurrency, :match_schedule, [schedule]) end)
-			_ -> IO.puts(:stderr, "An error deserializing")
+			:ok -> pids = Enum.map(schedules, fn schedule ->
+					spawn_link(Concurrency, :match_schedule, [schedule]) end)
+			_ -> 
+				IO.puts(:stderr, "An error deserializing")
+				:error
 		end
-
   end
 
-	# ndt: Schedule	
-	def match_schedule(ndt) do 
+	# schedule: Schedule	
+	def match_schedule(schedule) do 
+
 		IO.puts("Trying Again")
-		{ok?, ndt_scheduled} = NaiveDateTime.from_iso8601(ndt["scheduled"]) 
+		{ok?, ndt_scheduled} = NaiveDateTime.from_iso8601(schedule["scheduled"]) 
+
+		#case ok? do  
+		#	:error -> raise ArgumentError, "Error parsing schedule"
+		#end
 
 		new_ndt = NaiveDateTime.local_now
 		trunc_ndt = NaiveDateTime.add(new_ndt, -new_ndt.second)
 
 		case ndt_scheduled do 
 			^trunc_ndt ->
-				IO.puts("Hello world from #{ndt["task"]}")
-				System.cmd("notify-send", [ndt["task"]])
+				IO.puts("Hello world from #{schedule["task"]}")
+				System.cmd("notify-send", [schedule["task"]])
 			_ -> 
 				:timer.sleep(1000)
-				match_schedule(ndt)
+				match_schedule(schedule)
 		end
-	end
 
+	#	receive do 
+	#		:update -> Process.exit(self(), :kill) 
+	#	end
+		
+	end
 end
